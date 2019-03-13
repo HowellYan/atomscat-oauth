ee4769ba7e6949e136168feda7786d8119385c75
HowellYang <1726474652@qq.com>
2019-03-13 22:18:21 +0800

    init

--- oauth-domain/src/main/java/com/atomscat/AppStart.java
+++ oauth-domain/src/main/java/com/atomscat/AppStart.java
@@ -2,10 +2,8 @@ package com.atomscat;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableAsync;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 /**
  * @author Howell Yang
@@ -13,12 +11,8 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @SpringBootApplication
 //启用JPA审计
 @EnableJpaAuditing
-//启用缓存
-@EnableCaching
 //启用异步
 @EnableAsync
-//启用自带定时任务
-@EnableScheduling
 public class AppStart {
 
     public static void main(String[] args) {
