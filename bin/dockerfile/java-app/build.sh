04442fef4d1b736f239514a80e380383b53e73f6
HowellYang <1726474652@qq.com>
2019-03-14 21:16:26 +0800

    init

--- bin/dockerfile/java-app/build.sh
+++ bin/dockerfile/java-app/build.sh
@@ -3,5 +3,5 @@ mvn clean install -DskipTests;
 cp ./target/root.jar ./bin/dockerfile/java-app
 docker build --rm -t atomscat-oauth ./bin/dockerfile/java-app/;
 
-# docker run --name atomscat-oauth -e JAVA_OPTIONS="-Duser.timezone=GMT+8 -Xms128m -Xmx256m -Dspring.profiles.active=prod" --link=atomscat-oauth-mysql:atomscat-oauth-mysql  -p 8888:8888 -d atomscat-oauth
+#运行： docker run --name atomscat-oauth -e JAVA_OPTIONS="-Duser.timezone=GMT+8 -Xms128m -Xmx256m -Dspring.profiles.active=prod" --link=atomscat-oauth-mysql:atomscat-oauth-mysql  -p 8888:8888 -d atomscat-oauth
 # curl http://127.0.0.1:8888/actuator/health
--- bin/dockerfile/mysql/build.sh
+++ bin/dockerfile/mysql/build.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 docker build -t atomscat-oauth-mysql .;
-# docker run --name atomscat-oauth-mysql -d -p 3306:3306 -v /data/docker_db/atomscat-oauth-mysql:/var/lib/mysql -e MYSQL_ROOT_PASSWORD=Aa123456  atomscat-oauth-mysql
+#运行： docker run --name atomscat-oauth-mysql -d -p 3306:3306 -v /data/docker_db/atomscat-oauth-mysql:/var/lib/mysql -e MYSQL_ROOT_PASSWORD=Aa123456  atomscat-oauth-mysql
