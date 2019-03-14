fcb220193aa20763a6f46eb32b8e6c31feaefc99
HowellYang <th15817161961@gmail.com>
2019-03-14 16:42:58 +0800

    init

--- bin/dockerfile/java-app/build.sh
+++ bin/dockerfile/java-app/build.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
+mvn clean install -DskipTests;
 cp ./target/root.jar ./bin/dockerfile/java-app
-docker build -t atomscat-oauth ./bin/dockerfile/java-app/;
+docker build --rm -t atomscat-oauth ./bin/dockerfile/java-app/;
 
-#
+# docker run --name atomscat-oauth -e JAVA_OPTIONS="-Duser.timezone=GMT+8 -Xms128m -Xmx256m -Dspring.profiles.active=prod" --link=atomscat-oauth-mysql:atomscat-oauth-mysql  -p 8888:8888 -d atomscat-oauth
+# curl http://127.0.0.1:8888/actuator/health
